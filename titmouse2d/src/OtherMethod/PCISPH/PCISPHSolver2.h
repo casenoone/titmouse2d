18f355f56b29c5146872512af7fd132037c96da2
健 张 <787502862@qq.com>
2022-01-25 14:33:33 +0800

    add PCISPH algorithm

---
+++ titmouse2d/src/OtherMethod/PCISPH/PCISPHSolver2.h
@@ -0,0 +1,13 @@
+#ifndef PCISPHSOLVER2_H
+#define PCISPHSOLVER2_H
+
+#include "../../Lagrangian/SphSystemSolver2.h"
+
+class PCISPHSolver2 : public SphSystemSolver2 {
+public:
+	PCISPHSolver2() = default;
+}
+
+
+
+#endif
